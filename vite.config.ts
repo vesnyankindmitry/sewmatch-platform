8262f8cabb6246a4512cc757356fee950adb06f4
vesnyankindmitry <vesnyankindmitry@gmail.com>
2026-07-07 14:40:21 +0300

    fix: remove non-existent plugin-inspect-react-code

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'plugin-inspect-react-code'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [inspectAttr(), react()],
-  server: {
-    port: 3000,
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
